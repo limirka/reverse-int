db5f97662548fbb4fa582cfab64a90e8c5cd9762
Elizaveta Mirchuk <limirka@icloud.com>
2022-08-08 12:46:22 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n =  Math.abs(n)
+    n = String(n)
+    console.log(n);
+    let revNum = ""
+    for(let i = n.length - 1; i >= 0; i--){
+        revNum += n[i]
+    }
+    return parseInt(revNum)
 }
+
